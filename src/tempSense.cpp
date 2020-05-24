e8607690d6d0d167c38717b6dea6c9a8c77e5eff
Raymond Bowen <raymo@blackpanther.broadleaf.local>
2020-05-24 19:59:09 -0400

    first commit

---
+++ src/tempSense.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "f:/Code/tempSense/src/tempSense.ino"
+/*
+ * Project tempSense
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "f:/Code/tempSense/src/tempSense.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
